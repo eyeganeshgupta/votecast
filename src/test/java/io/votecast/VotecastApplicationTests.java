c8c0a5d362c0badc28aa3b91d341e76550af2b50
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-04-29 09:00:57 +0530

    spring initializr

---
+++ src/test/java/io/votecast/VotecastApplicationTests.java
@@ -0,0 +1,13 @@
+package io.votecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
